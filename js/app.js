c6b8e3ad6823b7d17a58a7a279886e325f4f3f9c
Anthony Hastings <anthony@rehabstudio.com>
2016-06-28 09:51:35 +0100

    Small change to pathing.

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/js/";
+/******/ 	__webpack_require__.p = "./js/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
