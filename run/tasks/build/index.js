7852b80d2916b4d0b92cbf35262d6284b44d3b4b
Anthony Hastings <anthony@rehabstudio.com>
2016-06-28 09:45:56 +0100

    Adding missing build task.

---
+++ run/tasks/build/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Builds all necessary front-end static files and moves fonts
+ * into the distribution folder also. This method is primarily
+ * used during deployment or initial setup.
+ *
+ * Example Usage:
+ * gulp build
+ */
+
+var gulp = require('gulp');
+var chalk = require('chalk');
+var globalSettings = require('../../config');
+
+gulp.task('build', ['html', 'images', 'styles', 'scripts'], function() {
+    if (Object.keys(globalSettings.taskConfiguration.scripts.webpackSettings.entry).length === 0) {
+        console.log(chalk.bgYellow.gray(' FE Skeleton: Warning - There are no script bundles defined.'));
+    }
+
+    if (globalSettings.taskConfiguration.styles.bundles.length === 0) {
+        console.log(chalk.bgYellow.gray(' FE Skeleton: Warning - There are no style bundles defined.'));
+    }
+
+    return gulp.src(globalSettings.taskConfiguration.build.sourcePaths, {base: './'})
+               .pipe(gulp.dest(globalSettings.destPath));
+});
